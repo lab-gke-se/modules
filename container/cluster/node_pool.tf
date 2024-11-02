a68663457f117bb8f21999d56c5f3204cc13c558
Dave Butland <dgbutland@gmail.com>
2024-11-02 00:49:01 +0000

    Changed cluster reference

--- container/cluster/node_pool.tf
+++ container/cluster/node_pool.tf
@@ -4,7 +4,7 @@ resource "google_container_node_pool" "node_pool" {
 
   # Terraform / cluster values
   project  = var.project
-  cluster  = google_container_cluster.cluster.name
+  cluster  = google_container_cluster.cluster.id
   location = var.location
   # name_prefix = var.name_prefix
 
