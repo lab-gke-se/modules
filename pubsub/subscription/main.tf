d8027bf34fe5d3958ca9ae90801ce6c3ca5040e1
Dave Butland <dgbutland@gmail.com>
2025-02-11 23:24:42 +0000

    Fixed typos

--- pubsub/subscription/main.tf
+++ pubsub/subscription/main.tf
@@ -82,7 +82,7 @@ resource "google_pubsub_subscription" "subscription" {
     for_each = try(var.expirationPolicy, null) != null ? [var.expirationPolicy] : []
 
     content {
-      ttl = try(expiriation_policy.value.ttl, null)
+      ttl = try(expiration_policy.value.ttl, null)
     }
   }
 
